6f1da3a0c592cf8fa356dd3b9bff90f050ee4fe6
LYZAcell <applelgy@naver.com>
2024-09-30 18:03:44 +0900

    로고 업로드

--- src/home/homepage.jsx
+++ src/home/homepage.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import Logo from "../common/logo";
 
 export default function MainHome(){
     return(
         <>
+        <Logo/>
         <h1>Test Test</h1>
         <h2>누구 있습니까?</h2>
         </>
