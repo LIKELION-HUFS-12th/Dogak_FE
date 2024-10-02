beaa3bb8efa41656e28903d38f2775dca3f9e28a
Chaeri-1 <alfsdo03@hufs.ac.kr>
2024-10-03 00:13:08 +0900

    login 편집

--- src/common/logo.jsx
+++ src/common/logo.jsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 30px;
+
+    img {
+        object-fit: cover;
+        width: 250px;
+    }
+`
+
 export default function Logo(){
     return(
-        <>
+        <LogoContainer>
             <img className = "LogoImg" alt= "MainLogo" src = "../../public/Logo_Dogak.svg"></img>
-        </>
+        </LogoContainer>
     );
 }
